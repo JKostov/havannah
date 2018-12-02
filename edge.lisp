eb1da17a1840aabf2d2f80faa4dbe50d2b3a4dcf
JKostov <julijekos@gmail.com>
2018-12-02 22:56:38 +0100

    Sides + helper

--- edge.lisp
+++ edge.lisp
@@ -21,22 +21,39 @@
     )
 )
 
-(defun generateSides (state numberOfCells)
+(defun generateSides (numberOfCells)
     (setq *sides* 
         (removeAllElements *edges*
             (append
+                (list (getSideListPlusNumber (+ 65 numberOfCells) 1 numberOfCells))
+                (list (getSideListPlusLetterDown (+ 65 numberOfCells) (* 2 (1- numberOfCells)) numberOfCells))
                 (list (generateSideList 65 1 (- numberOfCells 2)))
-                (list (getFirstAndLastElementFromState state))
+                (list (getSideListPlusLetter 66 0 numberOfCells))
+                (list (getSideListPlusLetter 66 (1- numberOfCells) numberOfCells))
                 (list (generateSideList (+ 65 (* 2 (1- numberOfCells))) numberOfCells (1- (* 2 (1- numberOfCells))) ))
             )
         )
     )
 )
 
-(defun getFirstAndLastElementFromState (state)
+(defun getSideListPlusLetter (letter number numberOfCells)
     (cond
-        ( (null state) '() )
-        ( t (append (list (list (caar state) (car (caadar state)))) (list (list (caar state) (caar (last (cadar state))))) (getFirstAndLastElementFromState (cdr state))))
+        ( (= letter (+ 65 (1- numberOfCells))) '() )
+        (t (append (list (list (string (code-char letter)) number)) (getSideListPlusLetter (1+ letter) number numberOfCells) ))
+    )
+)
+
+(defun getSideListPlusNumber (letter number numberOfCells)
+    (cond
+        ( (= letter (+ 65 (* 2 (1- numberOfCells)))) '() )
+        (t (append (list (list (string (code-char letter)) number)) (getSideListPlusNumber (1+ letter) (1+ number) numberOfCells) ))
+    )
+)
+
+(defun getSideListPlusLetterDown (letter number numberOfCells)
+    (cond
+        ( (= letter (+ 65 (* 2 (1- numberOfCells)))) '() )
+        (t (append (list (list (string (code-char letter)) number)) (getSideListPlusLetterDown (1+ letter) number numberOfCells) ))
     )
 )
 
@@ -48,6 +65,10 @@
     )
 )
 
-
-;; (print (generateEdges 6))
-;; (print (generateSides '() 6))
+(defun checkMoveIsSideAndRemoveSide (sides move)
+    (cond
+        ( (null sides) '() )
+        ( (member move (car sides) :test 'equal) (checkMoveIsSideAndRemoveSide (cdr sides) move) )
+        ( t (cons (car sides) (checkMoveIsSideAndRemoveSide (cdr sides) move)) )
+    )
+)
